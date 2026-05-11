8ba79ba7d014279288eaf0ef4a42849ce2c31e54
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2026-05-11 15:35:15 +0200

    Finished now, done

--- AmobaGame/AmobaGame.UI/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AmobaGame.UI.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
--- AmobaGame/obj/Debug/net10.0/AmobaGame.AssemblyInfo.cs
+++ AmobaGame/obj/Debug/net10.0/AmobaGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmobaGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cef4465e10444cf4fac901e3c944b22f1ced56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71f36aca0deeda14c40260a43f213b4894d1740")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmobaGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmobaGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
